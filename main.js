d84487de964fa3521f1c254325e149000bc5f454
chiaralle <chiaraallegrino@gmail.com>
2025-01-15 15:33:28 +0100

    add: funzione generaNumero

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ Minitask:
 
 */
 
+/* 
 let inserisciParola = prompt ("inserisci una parola")
 
 function isPalindrome(x){
@@ -38,10 +39,29 @@ if(isPalindrome(inserisciParola) == true) {
     console.log(`La parola ${inserisciParola} non è palidroma`)
 }
 
- 
+ */
 
 
-/*Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+/*Pari e Dispari 
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+ Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 Consigli del giorno
 Scriviamo sempre in italiano i passaggi che vogliamo fare
 Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti. */
+
+/*
+Mini task: 
+-creo una variabile con prompt per scegliere un numero da 1 a 5. 
+-creo una funzione usando la proprietà Math.floor(Math.random() * 10) + 1; per generare un numero per il computer da 1 a 5.
+-creo una funzione per avere il risultato della somma dei due numeri e, tramite il modulo %, stabilisco se il numero è pari o dispari.
+-stampo il risultato con il vincitore
+*/
+
+let inserisciUnNumero = prompt ("inserisci un numero da 1 a 5");
+
+function generaNumero(min, max){
+    let result = Math.floor(Math.random() * max) + min; 
+    return result;   
+}
+
+console.log(generaNumero(1, 5));
